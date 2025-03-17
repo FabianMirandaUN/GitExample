4dcf37a06877a90eb2720494d0b723502036abf2
Fabian De Jesus Miranda Varelo <fvarelo@uninorte.edu.co>
2025-03-17 15:47:05 -0500

    Feat : Added Dog class

--- src/core/Dog.java
+++ src/core/Dog.java
@@ -9,5 +9,19 @@ package core;
  * @author fvarelo
  */
 public class Dog {
-    
+
+    private String name;
+    private int age;
+    private double weight;
+    private double height;
+    private double race;
+
+    public Dog(String name, int age, double weight, double height, double race) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+        this.race = race;
+    }
+
 }
